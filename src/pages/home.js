6088cb624f48f9d59e5a611ef459ee7fe3ff3515
Johannes Frank <johannes.f002@gmail.com>
2020-10-09 14:00:25 +0200

    socialape-client 06:38:00

---
+++ src/pages/home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+import Scream from '../components/Scream'
+
+import Grid from '@material-ui/core/Grid';
+class home extends Component {
+    state= {
+        screams: null
+    }
+    componentDidMount(){
+        axios.get('/screams')
+        .then(res => {
+            this.setState({
+                screams:res.data
+            })
+        })
+        .catch(err => console.log(err))
+    }
+    render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream}/>)
+        ) : <p>Loading...</p>
+        return (
+            <Grid container spacing={4}>
+                <Grid item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default home
---
+++ src/util/theme.js
@@ -0,0 +1,47 @@
+export default {
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#00bcd4',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+    spreadThis: {
+      typography: {
+        useNextVariants: true
+      },
+      form:{
+        textAlign: 'center'
+      },
+      image: {
+          margin: '20px auto 20px auto'
+      },
+      pageTitle: {
+          margin: '10px auto 10px auto'
+      },
+      textField: {
+          margin: '10px auto 10px auto'
+      },
+      button: {
+          marginTop: 20,
+          position: 'relative',
+  
+  
+      },
+      customError:{
+          color: 'red',
+          fontSize: '0.8rem',
+          marginTop: 10
+      },
+      progress:{
+          position: 'absolute'
+      }
+    }
+  }
